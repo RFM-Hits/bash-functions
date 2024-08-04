670ebfa5eafbf5e30fda8f3be5e5ca6ffb97eea9
Rik Visser <r.visser@rikvissermedia.nl>
2024-08-04 18:58:26 +0200

    chore: Update common-functions.sh with privilege and package management functions

--- common-functions.sh
+++ common-functions.sh
@@ -74,3 +74,158 @@ function rpi_model() {
         exit 1
     fi
 }
+
+# Check user privileges
+# Takes one parameter: "privileged" to check if the user is root, "regular" to check if the user is not root.
+function check_privileges() {
+    if [[ $1 == "privileged" ]]; then
+        if [[ $EUID -ne 0 ]]; then
+            echo -e "${RED}This script must be run as root.${WHITE}"
+            exit 1
+        fi
+    elif [[ $1 == "regular" ]]; then
+        if [[ $EUID -eq 0 ]]; then
+            echo -e "${RED}This script must be run as a regular user.${WHITE}"
+            exit 1
+        fi
+    fi
+}
+
+
+# Check if the 'apt' package manager is present.
+# No parameters.
+function check_apt() {
+    if ! command -v apt > /dev/null 2>&1; then
+        echo -e "${RED}Error: apt is not installed. Exiting...${NC}"
+        exit 1
+    fi
+}
+
+# Update the OS using 'apt' package manager.
+# Parameters:
+# $1 - (Optional) The first argument, which should be "silent" to suppress output.
+function update_os() {
+    check_apt
+    if is_silent $1; then
+        echo -e "${BLUE}►► Updating all OS packages in silent mode...${NC}"
+        output_redirection="> /dev/null 2>&1"
+    else
+        echo -e "${BLUE}►► Updating all OS packages...${NC}"
+        output_redirection=""
+    fi
+    eval "apt -qq -y update $output_redirection"
+    eval "apt -qq -y full-upgrade $output_redirection"
+    eval "apt -qq -y autoremove $output_redirection"
+}
+
+# Installs packages using 'apt' package manager.
+# Parameters:
+# $1 - (Optional) The first argument, which should be "silent" to suppress output.
+# $@ - All the arguments, which should be the names of packages to install.
+function install_packages() {
+    if is_silent $1; then
+        output_redirection='> /dev/null 2>&1'
+        shift
+    else
+        output_redirection=''
+    fi
+    
+    check_apt
+    echo -e "${BLUE}►► Installing dependencies...${NC}"
+    eval "apt -qq -y update ${output_redirection}"
+    for package in "$@"; do
+        eval "apt -qq -y install ${package} ${output_redirection}"
+    done
+}
+
+# Set the system timezone.
+# Parameters:
+# $1 - The first argument, which should be a valid timezone, e.g. "Europe/Amsterdam".
+function set_timezone() {
+    local timezone=$1
+    if [ -f "/usr/share/zoneinfo/${timezone}" ]; then
+        echo -e "${BLUE}►► Setting timezone to ${timezone}...${NC}"
+        ln -fs /usr/share/zoneinfo/$timezone /etc/localtime > /dev/null
+        dpkg-reconfigure -f noninteractive tzdata > /dev/null
+    else
+        echo -e "${RED} Error: Invalid timezone: ${timezone}${NC}"
+    fi
+}
+
+# Checks the installation of packages that provide required commands
+# Parameters:
+# $@ - All the arguments, which should be the names of the commands to check.
+function check_required_command {
+  for cmd in "$@"; do
+    if ! command -v "$cmd" &> /dev/null; then
+      echo -e "${RED} Error: Installation failed. $cmd is not successfully installed.${NC}"
+      INSTALL_FAILED=true
+    fi
+  done
+}
+
+# Prompt the user for input.
+# If the user doesn't provide a value, the default value is assigned.
+# Parameters:
+# $1 - The variable name (will be all caps)
+# $2 - The default value for the variable
+# $3 - The prompt to display to the user
+# $4 - (Optional) The type of the variable (y/n, num, str, email, host). Default is str.
+# Example:
+# ask_user "MY_NUM" "1" "Please enter a number" "num"
+function ask_user {
+  local var_name="$1"
+  local default_value="$2"
+  local prompt="$3"
+  local var_type="${4:-str}"
+
+  local input
+
+  while true; do
+    read -p "${prompt} [default: ${default_value}]: " input
+    input="${input:-$default_value}"
+
+    case $var_type in
+      'y/n')
+        if [[ "$input" =~ ^(y|n)$ ]]; then
+          break
+        else
+          echo "Invalid input. Please enter y or n."
+        fi
+        ;;
+      'num')
+        if [[ "$input" =~ ^[0-9]+$ ]]; then
+          break
+        else
+          echo "Invalid input. Please enter a number."
+        fi
+        ;;
+      'str')
+        if [[ -n "$input" ]]; then
+          break
+        else
+          echo "Invalid input. Please enter a string."
+        fi
+        ;;
+      'email')
+        if [[ "$input" =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
+          break
+        else
+          echo "Invalid input. Please enter a valid e-mail address."
+        fi
+        ;;
+      'host')
+        if [[ "$input" =~ ^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$ ]]; then
+          break
+        else
+          echo "Invalid input. Please enter a valid hostname."
+        fi
+        ;;  
+      *)
+        echo "Unknown validation type: $var_type"
+        return 1
+        ;;
+    esac
+  done
+
+  eval "$var_name=\"$input\""

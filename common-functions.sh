23f24ccab68091fdb0a7f975d103a7dc094b3a06
Rik Visser <r.visser@rikvissermedia.nl>
2024-08-04 18:52:11 +0200

    chore: Add common-functions.sh for terminal color variables

---
+++ common-functions.sh
@@ -0,0 +1,18 @@
+# Init color variables for terminal output
+function set_colors() {
+    # Regular Colors
+    BLACK='\033[0;30m'
+    WHITE='\033[1;37m'
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    BLUE='\033[0;34m'
+    
+    # Bold Colors
+    BBLACK='\033[1;30m'
+    BWHITE='\033[1;37m'
+    BRED='\033[1;31m'
+    BGREEN='\033[1;32m'
+    BYELLOW='\033[1;33m'
+    BBLUE='\033[1;34m'
+}
